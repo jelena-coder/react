a5ac2bea6aa59c51f02ca928e46cb7393e70f9e6
Jelena Slijepcevic <jeca.lelica9983@gmail.com>
2022-02-05 02:53:53 +0100

    napravila boje da se menjaju

---
+++ src/boje.js
@@ -0,0 +1,9 @@
+
+const plaviBrojevi = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+export const getBoja = broj => {
+  if (broj === 0) {
+    return 'pink';
+  }
+  return crveniBrojevi.includes(broj) ? 'blue' : 'white';
+}
