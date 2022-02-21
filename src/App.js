d0a1e007f1bf1b5495ca8fa2566a3e4701cfae44
Jelena Slijepcevic <jeca.lelica9983@gmail.com>
2022-02-21 22:08:31 +0100

    dodate klase za boje u css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import { useState } from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, BrowserRouter, Routes } from 'react-router-dom'; 
 import './App.css';
 import Navbar from './components/Navbar';
 import Igra from './pages/Igra';
